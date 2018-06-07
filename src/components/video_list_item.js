f48d0fbfa5078499c1df8cd9c52c370cc18b4e74
Brendan Koral <bkoral@dealerinspire.com>
2018-06-07 16:42:55 -0500

    Add some work on video list item

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -4,7 +4,23 @@ const VideoListItem = ({video}) => {
     //Passing in an object with the property is the same as the below
     // const video = props.video;
 
-    return <li>Video</li>
+    const imageUrl = video.snippet.thumbnails.default.url;
+
+    return (
+        <li className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl} />
+                </div>
+
+                <div className="media-body">
+                    <div className="media-heading">
+                        {video.snippet.title}
+                    </div>
+                </div>
+            </div>
+        </li>
+    );
 }
 
 export default VideoListItem;
